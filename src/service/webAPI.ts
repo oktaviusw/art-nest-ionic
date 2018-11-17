393417f172ef78703e302482ab6c2f1f5d55504b
annajeane <annajeane@gmail.com>
2018-11-17 19:23:51 +0700

    Listing API for WebService
    + Nulis List di API Webhostnya

--- src/service/webAPI.ts
+++ src/service/webAPI.ts
@@ -10,8 +10,33 @@ export class APIService
 {
     public readonly API_SERVER  = 'https://artnest-umn.000webhostapp.com/API/'
 
-    public readonly USERS_REGISTER  = this.API_SERVER + 'RegisterUser/';
-    public readonly USERS_LOGIN     = this.API_SERVER + 'LoginUser/';
+    public readonly USERS_REGISTER      = this.API_SERVER + 'RegisterUser/';
+    public readonly USERS_LOGIN         = this.API_SERVER + 'LoginUser/';
+    public readonly USERS_DATA          = this.API_SERVER + 'LoadSingleUserData/';
+    public readonly USERS_UPDATE        = this.API_SERVER + 'UpdateUserData/';
+
+    public readonly ARTIST_REGISTER     = this.API_SERVER + 'BecomeArtist/';
+    public readonly ARTIST_DATA_ALL     = this.API_SERVER + 'ShowAllArtist/';
+    public readonly ARTIST_DATA_SINGLE  = this.API_SERVER + 'LoadArtistData/';
+    public readonly ARTIST_UPDATE       = this.API_SERVER + 'UpdateArtistData/';
+
+    public readonly CATEGORY_ALL        = this.API_SERVER + 'GetAllCategories/';
+    public readonly CATEGORY_AVAILABLE  = this.API_SERVER + 'GetAvailableCategory/';
+    public readonly CATEGORY_ADD        = this.API_SERVER + 'UpdateAddCategory/';
+    public readonly CATEGORY_DELETE     = this.API_SERVER + 'UpdateDeleteCategory/';
+
+    public readonly ARTWORK_DATA_ALL    = this.API_SERVER + 'ShowAllArtowrks/';
+    public readonly ARTWORK_DATA_SINGLE = this.API_SERVER + 'LoadArtworkData/';
+    public readonly ARTWORK_ADD         = this.API_SERVER + 'AddNewArtwork/';
+    public readonly ARTWORK_DELETE      = this.API_SERVER + 'DeleteArtwork/';
+    public readonly ARTWORK_UPDATE      = this.API_SERVER + 'UpdateArtworkData/';
+
+    public readonly REQUEST_DATA_ALL    = this.API_SERVER + 'ShowAllRequest/';
+    public readonly REQUEST_DATA_SINGLE = this.API_SERVER + 'LoadCommissionData/';
+    public readonly REQUEST_ADD         = this.API_SERVER + 'AddNewCommission/';
+    public readonly REQUEST_RESPONSE    = this.API_SERVER + 'RespondToCommission/';
+
+    public readonly UTIL_SEARCH         = this.API_SERVER + 'SeachData/';
 
     constructor(private _sanitizer: DomSanitizer, private http: HttpClient, private alertCtrl: AlertController){
 
