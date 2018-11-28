a5cd46b52d146644facd65a8a58599311ced743c
brandon-tanujaya <39830148+brandon-tanujaya@users.noreply.github.com>
2018-11-28 15:39:29 +0700

    HomePage, test pull image data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { APIService } from '../../service/webAPI';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
+  artworks =[];
 
+  constructor(public navCtrl: NavController, private api: APIService, public sanitizer: DomSanitizer) {
+
+  }
+
+  ngOnInit() {
+    this.api.getAPI("https://artnest-umn.000webhostapp.com/API/ShowAllArtworks")
+      .map(response =>{
+        console.log(response);
+        this.artworks = response.result;
+        console.log(this.artworks);
+      }).subscribe();
+  }
+
+  checkArtworks() {
+    if(this.artworks.length > 0){
+      return true;
+    }
   }
 
 }
