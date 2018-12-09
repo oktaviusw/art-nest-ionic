46ad1ac325d0962d1249553e6fc863fa74bea380
annajeane <annajeane@gmail.com>
2018-12-09 19:29:28 +0700

    Simpan ID User Login
    + Simpen user ID Login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -57,6 +57,7 @@ export class LoginPage {
       console.log(response);
       
       if(response.status == "OK"){
+        this.storage.set('idUserSQL',response.result);
         //Login Firebase
         this.firebaseProvider.loginUser(this.loginForm.value.email, this.loginForm.value.password)
         .then( loginUser => {
