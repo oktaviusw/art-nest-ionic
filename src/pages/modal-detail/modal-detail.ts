dede28b327ce5513443053e9dbfe47afc67d305e
brandon-tanujaya <39830148+brandon-tanujaya@users.noreply.github.com>
2018-12-12 19:24:55 +0700

    Request + Project Page connect to ModalDetails

--- src/pages/modal-detail/modal-detail.ts
+++ src/pages/modal-detail/modal-detail.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { APIService } from '../../service/webAPI';
 
 /**
  * Generated class for the ModalDetailPage page.
@@ -13,9 +14,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-modal-detail',
   templateUrl: 'modal-detail.html',
 })
-export class ModalDetailPage {
+export class ModalDetailPage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  comission:any;
+  comissionDetails = [];
+  comission_id = 0;
+  loading:any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+            public loadingCtrl: LoadingController, public api: APIService) {
+
+  }
+
+  ngOnInit(){
+
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.loading.present();
+
+    this.comission = this.navParams.get('comission');
+    this.comission_id = this.comission.IDProject;
+    console.log(this.comission_id);
+
+    //Get single data from artist
+    this.api.getAPI(this.api.REQUEST_DATA_SINGLE + this.comission_id)
+      .map(response =>{
+        this.comissionDetails = response.result;
+        console.log(this.comissionDetails);
+        //Data is now loaded; dismiss load anim
+        this.loading.dismiss();
+      }).subscribe();
   }
 
   ionViewDidLoad() {
