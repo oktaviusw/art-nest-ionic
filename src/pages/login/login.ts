10b0d342d05c4cc62813395656a191ed6fdb7364
brandon-tanujaya <39830148+brandon-tanujaya@users.noreply.github.com>
2018-12-12 13:10:20 +0700

    push sampah (pls ignore)

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -65,7 +65,7 @@ export class LoginPage {
       if(response.status == "OK"){
 
         this.storage.set('idUserSQL', response.result);
-        //this.api.loggedInUser = response.result;
+        this.api.loggedInUser = response.result;
         this.events.publish('userLoggedIn', response.result);
 
         //Login Firebase
